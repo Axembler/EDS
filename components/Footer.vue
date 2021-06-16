4e22ca68a5fed11edfe04f34966fa04a4bd77023
Axembler <timur.payuk.axembler@mail.ru>
2021-06-16 20:57:16 +0300

    обратно

--- components/Footer.vue
+++ components/Footer.vue
@@ -26,21 +26,21 @@
         <p>Разработка и дизайн сайта</p>
         <p>— ОБУ «ИТЦ» 2020</p>
       </div>
-      <noscript><div><img src="https://mc.yandex.ru/watch/81013720" alt="" /></div></noscript>
+      <!-- <noscript><div><img src="https://mc.yandex.ru/watch/81013720" alt="" /></div></noscript> -->
     </div>
   </footer>
 </template>
 
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ <script type="text/javascript">
+//    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+//    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+//    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(81013720, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
+//    ym(81013720, "init", {
+//         clickmap:true,
+//         trackLinks:true,
+//         accurateTrackBounce:true
+//    });
 </script>
 
 <style scoped>
