0c3b6c86fa69534290269824f15566eb237ec5af
Axembler <timur.payuk.axembler@mail.ru>
2021-06-16 20:21:17 +0300

    тест

--- components/Footer.vue
+++ components/Footer.vue
@@ -26,22 +26,9 @@
         <p>Разработка и дизайн сайта</p>
         <p>— ОБУ «ИТЦ» 2020</p>
       </div>
-      <noscript><div><img src="https://mc.yandex.ru/watch/81013720" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     </div>
   </footer>
 </template>
-
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(81013720, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
-</script>
 
 <style scoped>
 footer {
--- pages/index.vue
+++ pages/index.vue
@@ -50,6 +50,7 @@
       <p><span class="outcome-numbers">717</span><span>Принято звонков<br> за неделю</span></p>
       <p><span class="outcome-numbers">673</span><span>Выполнено заявок<br> за неделю</span></p>
     </div>
+    <noscript><div><img src="https://mc.yandex.ru/watch/81013720" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <div class="map">
       <div class="map-dis">
         <h1>Карта отключений</h1>
@@ -62,6 +63,18 @@
 
 <script>
 export default {}
+</script>
+
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(81013720, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
 </script>
 
 <style>
