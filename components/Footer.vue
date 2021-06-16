26f4d084e0d967c979e79d202a7e2ae7d197c2b2
Axembler <timur.payuk.axembler@mail.ru>
2021-06-16 21:11:30 +0300

    ну что, заработает?

--- components/Footer.vue
+++ components/Footer.vue
@@ -26,22 +26,9 @@
         <p>Разработка и дизайн сайта</p>
         <p>— ОБУ «ИТЦ» 2020</p>
       </div>
-      <!-- <noscript><div><img src="https://mc.yandex.ru/watch/81013720" alt="" /></div></noscript> -->
     </div>
   </footer>
 </template>
-
- <script type="text/javascript">
-//    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-//    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-//    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-//    ym(81013720, "init", {
-//         clickmap:true,
-//         trackLinks:true,
-//         accurateTrackBounce:true
-//    });
-</script>
 
 <style scoped>
 footer {
@@ -104,6 +91,8 @@ footer {
     flex-direction: column;
     align-items: center;
     text-align: center;
+  }
+  .mail {
     padding-bottom: 15px;
   }
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="main">
+    <noscript><div><img src="https://mc.yandex.ru/watch/81013720" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <Header />
     <Nuxt />
     <Footer />
   </div>
 </template>
+
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(81013720, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
 
 <style>
 *,
